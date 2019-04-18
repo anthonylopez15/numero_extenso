fd67a093806247e56a51596e6b77289cd39559f3
Anthony Lopez <tony.lopez.15@gmail.com>
2019-04-18 10:01:03 -0400

    criando um model de exemplo

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Numero(models.Model):
+    numero = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.numero
