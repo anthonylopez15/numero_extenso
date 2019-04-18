89e1d81b60b35ff12e2204485189a959855cb2db
Anthony Lopez <tony.lopez.15@gmail.com>
2019-04-18 10:02:37 -0400

    expondo a url com django rest

--- num_extenso/urls.py
+++ num_extenso/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+from django.conf.urls import include
+from core.viewset import NumeroViewSet
+
+router = routers.DefaultRouter()
+router.register(r'', NumeroViewSet, base_name='numero')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
