3db67832eedb136203c60789b4271b990c23f65f
Anthony Lopez <tony.lopez.15@gmail.com>
2019-04-18 10:03:35 -0400

    criando metodo pra pegar o get da url e converter

--- core/viewset.py
+++ core/viewset.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from core.num_extenso import convert_value
+from core.serializers import NumeroSerializer
+
+
+class NumeroViewSet(ModelViewSet):
+    serializer_class = NumeroSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+
+        data = kwargs
+        try:
+            result = convert_value(data['pk'])
+            data = {
+                'extenso': result
+            }
+            return Response(data, status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e.args[0])
+            return Response(status=status.HTTP_400_BAD_REQUEST)
